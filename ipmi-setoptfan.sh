6b97bebe411e0853646e8d96cad31bec51e6b1ef
klimvoroshilov13 <klimvoroshilov13@gmail.com>
2024-10-30 20:47:58 +0300

    wg 0.2, ipmi-setfun 0.12

--- ipmi-setoptfan.sh
+++ ipmi-setoptfan.sh
@@ -6,4 +6,4 @@ USER=""
 PASS=""
 
 # Commands
-ipmitool -I lanplus -U $USER -P $PASS -H $IP raw 0x30 0x45 0x01 0x02
+ipmitool -I lanplus -U $USER -P $PASS -H $IP raw 0x30 0x45 0x01 0x02 > /dev/null 2>&1
